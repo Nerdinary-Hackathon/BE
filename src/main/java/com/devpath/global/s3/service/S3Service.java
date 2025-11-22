702ca21382be189506b540cce48f8b3637b87fb0
김준호 <2171168@hansung.ac.kr>
2025-11-22 23:17:23 +0900

    feat: presigned 방식 코드 추가

---
+++ src/main/java/com/devpath/global/s3/service/S3Service.java
@@ -0,0 +1,44 @@
+package com.devpath.global.s3.service;
+
+import com.devpath.global.s3.dto.S3Dto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final S3Presigner s3Presigner;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public S3Dto.PreSignedUrlResponse getPreSignedUrl(String prefix, String fileName) {
+        String key = prefix + "/" + UUID.randomUUID() + "_" + fileName;
+
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .putObjectRequest(objectRequest)
+                .build();
+
+        PresignedPutObjectRequest presignedRequest = s3Presigner.presignPutObject(presignRequest);
+
+        return S3Dto.PreSignedUrlResponse.builder()
+                .preSignedUrl(presignedRequest.url().toString())
+                .key(key)
+                .build();
+    }
+}
