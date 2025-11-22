45a7041adfcae290e3fa31aed5848f8bca157c33
jaehyeon4406 <jaehyeon4406@gmail.com>
2025-11-23 02:15:50 +0900

    refactor: 사용하지 않는 클래스 제거

--- src/main/java/com/devpath/domain/user/repository/TechStackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.devpath.domain.user.repository;
-
-import com.devpath.domain.user.entity.TechStack;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TechStackRepository extends JpaRepository<TechStack, Long> {
-}
-
